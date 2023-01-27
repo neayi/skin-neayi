609f28e34638d3a6836392157973f9bed3060709
Bertrand Gorge <bertrand.gorge@neayi.com>
2023-01-27 16:03:48 +0100

    working generator. Still missing some translations

--- generator/menus.php
+++ generator/menus.php
@@ -1,108 +1,105 @@
 <?php
 
-$topMenus = array();
-$topMenus['productions'] = "Productions";
-$topMenus['thematiques'] = "Thématiques";
-
-$productions = array();
-$productions['/wiki/Grandes cultures'] = 'Grandes cultures';
-$productions['/wiki/Élevage'] = 'Élevage';
-$productions['/wiki/Viticulture'] = 'Viticulture';
-$productions['/wiki/Arboriculture'] = 'Arboriculture';
-$productions['/wiki/Maraîchage'] = 'Maraîchage';
-$productions['/wiki/PPAM'] = 'PPAM';
-
-$subMenus = array();
-$subMenus['productions'] = array();
-
-foreach ($productions as $k => $v)
-    $subMenus['productions'][$k] = $v;
-
-$subMenus['productions'][''] = '-';
-$subMenus['productions']['/wiki/Search?Search?filters=A+un+type+de+page%5E%5EExemple+de+mise+en+œuvre&order=desc&ordertype=Modification+date'] = 'Rechercher des retours d’expérience';
-
-$thematiques = array();
-$thematiques['/wiki/Agriculture de précision'] = 'Agriculture de précision';
-$thematiques['/wiki/Agroforesterie'] = 'Agroforesterie';
-$thematiques['/wiki/Bioagresseurs'] = 'Bioagresseurs';
-$thematiques['/wiki/Biodiversité'] = 'Biodiversité';
-$thematiques['/wiki/Collaboration et coopération en agriculture'] = 'Collaboration et coopération en agriculture';
-$thematiques['/wiki/Couverts végétaux'] = 'Couverts végétaux';
-$thematiques['/wiki/Cycle du carbone et GES'] = 'Cycle du carbone et GES';
-$thematiques['/wiki/Désherbage'] = 'Désherbage';
-$thematiques['/wiki/Diagnostic et performance d\'une ferme'] = 'Diagnostic et performance';
-$thematiques['/wiki/Gestion de l’eau'] = 'Gestion de l’eau';
-$thematiques['/wiki/Matériel et machines agricoles'] = 'Matériel et machines agricoles';
-$thematiques['/wiki/Organisation du travail'] = 'Organisation du travail';
-$thematiques['/wiki/Outils de mesure et d’observation'] = 'Outils de mesure et d’observation';
-$thematiques['/wiki/Production énergétique'] = 'Production énergétique';
-$thematiques['/wiki/Protection intégrée des cultures'] = 'Protection intégrée des cultures';
-$thematiques['/wiki/Résilience climatique'] = 'Résilience climatique';
-$thematiques['/wiki/S\'installer en agriculture'] = 'S’installer';
-$thematiques['/wiki/Agronomie des sols vivants'] = 'Sols vivants';
-$thematiques['/wiki/Valorisation de la production'] = 'Valorisation de la production';
-
-
-$subMenus['thematiques'] = array();
-
-foreach ($thematiques as $k => $v)
-    $subMenus['thematiques'][$k] = $v;
-
-$subMenus['thematiques'][''] = '-';
-$subMenus['thematiques']['/wiki/Search?filters=A+un+type+de+page%5E%5EPratique&order=desc&ordertype=Modification+date'] = 'Rechercher des pratiques';
-
-
-$footerMenus = array();
-$footerMenus[1]["Triple Performance"]['/wiki/%C3%80_propos'] = "À propos";
-$footerMenus[1]["Triple Performance"]['/wiki/Partenaires'] = "Partenaires";
-$footerMenus[1]["Triple Performance"]['/wiki/Manifeste_de_la_plateforme_Triple_Performance'] = "Pourquoi ?";
-$footerMenus[1]["Triple Performance"]['/wiki/Comment_fonctionne_la_plateforme'] = "Comment ça fonctionne ?";
-$footerMenus[1]["Triple Performance"]['neayi-tour'] = "Lancer la visite guidée";
-$footerMenus[1]["Communauté"]['Special:Login'] = "Créer un compte ou se connecter";
-$footerMenus[1]["Communauté"]['/wiki/Aide:Bienvenue'] = "Contribuer&nbsp;? Ça commence ici&nbsp;!";
-$footerMenus[1]["Communauté"]['forum'] = "Accéder au forum";
-
-foreach ($productions as $k => $v)
-    $footerMenus[2]["Productions"][$k] = $v;
-
-$thematiquesFirstCol = array_slice($thematiques, 0, ceil(count($thematiques) / 2), true);
-$thematiquesSecondCol = array_slice($thematiques, ceil(count($thematiques) / 2), count($thematiques), true);
-
-foreach ($thematiquesFirstCol as $k => $v)
-    $footerMenus[3]["Thématiques"][$k] = $v;
-
-foreach ($thematiquesSecondCol as $k => $v)
-    $footerMenus[4][''][$k] = $v;
-
-$lastSection = 'Fait avec ♡ par <a target="_blank" href="https://neayi.com/">Neayi</a>';
-$footerMenus[5][$lastSection]['/wiki/Charte_d%E2%80%99utilisation'] = "Charte de la plateforme";
-$footerMenus[5][$lastSection]['/wiki/Triple_Performance:Politique_de_confidentialité'] = "Mentions légales";
-$footerMenus[5][$lastSection]['/wiki/Open_Source'] = "Open Source";
-$footerMenus[5][$lastSection]['contact'] = "Contact";
-$footerMenus[5][$lastSection]['socialLinks'] = "socialLinks";
-
-// Additional mobile menus
-$addMobileMenus = array();
-$addMobileMenus['/wiki/Productions'] = 'Productions';
-$addMobileMenus['/wiki/Thématiques'] = 'Thématiques';
-
-$socialLinks['fab fa-facebook-f'] = ['https://www.facebook.com/tripleperformance', "Suivez nous sur Facebook"];
-$socialLinks['fab fa-twitter'] = ['https://twitter.com/TriplePerforma1', "Suivez nous sur Twitter"];
-$socialLinks['fab fa-linkedin-in'] = ['https://www.linkedin.com/company/neayi', "Suivez nous sur LinkedIn"];
-
-$languages = array( 'fr' => 'Français',
-                    'en' => 'English',
-                    'de' => 'Deutsch',
-                    'pl' => 'Polski',
-                    'nl' => 'Nederlands',
-                    'it' => 'Italiano',
-                    'es' => 'Español');
-
 $xml_template = file_get_contents(__DIR__ . '/layout.template.xml');
-foreach ($languages as $langcode => $langname)
+
+$languages = glob(__DIR__ . '/i18n/*.json');
+
+foreach ($languages as $langfile)
 {
+    $langcode = str_replace('.json', '', basename($langfile));
+
     $contactLink = 'https://neayi.com/contact/';
     $forumLink = 'https://forum.'.$langcode.'.tripleperformance.ag';
+
+    $_ = json_decode(file_get_contents($langfile), true);
+
+    $topMenus = array();
+    $topMenus['productions'] = $_['productions'];
+    $topMenus['thematiques'] = $_['themes'];
+
+    $productions = array();
+    $productions['/wiki/Grandes cultures'] = $_['field-crops'];
+    $productions['/wiki/Élevage'] = $_['breeding'];
+    $productions['/wiki/Viticulture'] = $_['viticulture']; // 'Viticulture';
+    $productions['/wiki/Arboriculture'] = $_['arboriculture']; // 'Arboriculture';
+    $productions['/wiki/Maraîchage'] = $_['market-gardening']; // 'Maraîchage';
+    $productions['/wiki/PPAM'] = $_['hmp']; // 'PPAM';
+
+    $subMenus = array();
+    $subMenus['productions'] = array();
+
+    foreach ($productions as $k => $v)
+        $subMenus['productions'][$k] = $v;
+
+    $subMenus['productions'][''] = '-';
+    $subMenus['productions']['/wiki/Search?Search?filters=A+un+type+de+page%5E%5EExemple+de+mise+en+œuvre&order=desc&ordertype=Modification+date'] = $_['search-for-feedbacks']; // 'Rechercher des retours d’expérience';
+
+    $thematiques = array();
+    $thematiques['/wiki/Agriculture de précision'] = $_['precision-agriculture']; // 'Agriculture de précision';
+    $thematiques['/wiki/Agroforesterie'] = $_['agroforestry']; // 'Agroforesterie';
+    $thematiques['/wiki/Bioagresseurs'] = $_['bioaggressors']; // 'Bioagresseurs';
+    $thematiques['/wiki/Biodiversité'] = $_['biodiversity']; // 'Biodiversité';
+    $thematiques['/wiki/Collaboration et coopération en agriculture'] = $_['collaboration-and-cooperation-in-agriculture']; // 'Collaboration et coopération en agriculture';
+    $thematiques['/wiki/Couverts végétaux'] = $_['covers']; // 'Couverts végétaux';
+    $thematiques['/wiki/Cycle du carbone et GES'] = $_['carbon-cycle-and-ghg']; // 'Cycle du carbone et GES';
+    $thematiques['/wiki/Désherbage'] = $_['weeding']; // 'Désherbage';
+    $thematiques['/wiki/Diagnostic et performance d\'une ferme'] = $_['diagnosis-and-performance']; // 'Diagnostic et performance';
+    $thematiques['/wiki/Gestion de l’eau'] = $_['water-management']; // 'Gestion de l’eau';
+    $thematiques['/wiki/Matériel et machines agricoles'] = $_['agricultural-equipment-and-machinery']; // 'Matériel et machines agricoles';
+    $thematiques['/wiki/Organisation du travail'] = $_['work-organization']; // 'Organisation du travail';
+    $thematiques['/wiki/Outils de mesure et d’observation'] = $_['measurement-and-observation-tools']; // 'Outils de mesure et d’observation';
+    $thematiques['/wiki/Production énergétique'] = $_['energy-production']; // 'Production énergétique';
+    $thematiques['/wiki/Protection intégrée des cultures'] = $_['integrated-crop-protection']; // 'Protection intégrée des cultures';
+    $thematiques['/wiki/Résilience climatique'] = $_['climate-resilience']; // 'Résilience climatique';
+    $thematiques['/wiki/S\'installer en agriculture'] = $_['settling-down']; // 'S’installer';
+    $thematiques['/wiki/Agronomie des sols vivants'] = $_['living-soils']; // 'Sols vivants';
+    $thematiques['/wiki/Valorisation de la production'] = $_['valuation-of-production']; // 'Valorisation de la production';
+
+    $subMenus['thematiques'] = array();
+
+    foreach ($thematiques as $k => $v)
+        $subMenus['thematiques'][$k] = $v;
+
+    $subMenus['thematiques'][''] = '-';
+    $subMenus['thematiques']['/wiki/Search?filters=A+un+type+de+page%5E%5EPratique&order=desc&ordertype=Modification+date'] = $_['search-for-practices']; // 'Rechercher des pratiques';
+
+    $footerMenus = array();
+    $footerMenus[1]["Triple Performance"]['/wiki/%C3%80_propos'] = $_['about']; // "À propos";
+    $footerMenus[1]["Triple Performance"]['/wiki/Partenaires'] = $_['partners']; // "Partenaires";
+    $footerMenus[1]["Triple Performance"]['/wiki/Manifeste_de_la_plateforme_Triple_Performance'] = $_['why-triple-performance']; // "Pourquoi ?";
+    $footerMenus[1]["Triple Performance"]['/wiki/Comment_fonctionne_la_plateforme'] = $_['how-does-it-works']; // "Comment ça fonctionne ?";
+    $footerMenus[1]["Triple Performance"]['neayi-tour'] = $_['guided-tour']; // "Lancer la visite guidée";
+    $footerMenus[1][$_['community']]['Special:Login'] = $_['create-an-account-or-sign-in']; // "Créer un compte ou se connecter";
+    $footerMenus[1][$_['community']]['/wiki/Aide:Bienvenue'] = $_['to-contribute-start-here']; // "Contribuer&nbsp;? Ça commence ici&nbsp;!";
+    $footerMenus[1][$_['community']]['forum'] = $_['access-the-forum']; // "Accéder au forum";
+
+    foreach ($productions as $k => $v)
+        $footerMenus[2][$_['productions']][$k] = $v;
+
+    $thematiquesFirstCol = array_slice($thematiques, 0, ceil(count($thematiques) / 2), true);
+    $thematiquesSecondCol = array_slice($thematiques, ceil(count($thematiques) / 2), count($thematiques), true);
+
+    foreach ($thematiquesFirstCol as $k => $v)
+        $footerMenus[3][$_['themes']][$k] = $v;
+
+    foreach ($thematiquesSecondCol as $k => $v)
+        $footerMenus[4][''][$k] = $v;
+
+    $lastSection = $_['made_with_care']; // 'Fait avec ♡ par <a target="_blank" href="https://neayi.com/">Neayi</a>';
+    $footerMenus[5][$lastSection]['/wiki/Charte_d%E2%80%99utilisation'] = $_['platform-charter']; // "Charte de la plateforme";
+    $footerMenus[5][$lastSection]['/wiki/Triple_Performance:Politique_de_confidentialité'] = $_['legal-notice']; // "Mentions légales";
+    $footerMenus[5][$lastSection]['/wiki/Open_Source'] = $_['open-source']; // "Open Source";
+    $footerMenus[5][$lastSection]['contact'] = $_['contact']; // "Contact";
+    $footerMenus[5][$lastSection]['socialLinks'] = "socialLinks";
+
+    // Additional mobile menus
+    $addMobileMenus = array();
+    $addMobileMenus['/wiki/Productions'] = $_['productions']; // 'Productions';
+    $addMobileMenus['/wiki/Thématiques'] = $_['themes']; // 'Thématiques';
+
+    $socialLinks['fab fa-facebook-f'] = ['https://www.facebook.com/tripleperformance', $_['follow-us-on-facebook']];
+    $socialLinks['fab fa-twitter'] = ['https://twitter.com/TriplePerforma1', $_['follow-us-on-twitter']];
+    $socialLinks['fab fa-linkedin-in'] = ['https://www.linkedin.com/company/neayi', $_['follow-us-on-linkedin']];
 
     $localisedXML = $xml_template;
     foreach ($topMenus as $menuKey => $topMenuTitle)
@@ -141,7 +138,10 @@ MENU;
     $strFooterMenu = '';
     foreach ($footerMenus as $colNumber => $menus)
     {
-        $strFooterMenu .= '<div class="col-xl-2 col-lg-6">' . PHP_EOL;
+        if (empty($strFooterMenu))
+            $strFooterMenu .= '<div class="col-xl-2 col-lg-6 offset-xl-1">' . PHP_EOL;
+        else
+            $strFooterMenu .= '<div class="col-xl-2 col-lg-6">' . PHP_EOL;
 
         foreach ($menus as $section => $submenus)
         {
